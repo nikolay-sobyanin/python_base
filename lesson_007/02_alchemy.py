71d0a3217ffcae3d11c1b07f4e3a0f49e527898a
Nikolay <kolya-sob@yandex.ru>
2020-12-30 15:51:21 +0300

    Lesson 7. Exercise 02 done.

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -3,13 +3,6 @@
 # Создать прототип игры Алхимия: при соединении двух элементов получается новый.
 # Реализовать следующие элементы: Вода, Воздух, Огонь, Земля, Шторм, Пар, Грязь, Молния, Пыль, Лава.
 # Каждый элемент организовать как отдельный класс.
-# Таблица преобразований:
-#   Вода + Воздух = Шторм
-#   Вода + Огонь = Пар
-#   Вода + Земля = Грязь
-#   Воздух + Огонь = Молния
-#   Воздух + Земля = Пыль
-#   Огонь + Земля = Лава
 
 # Сложение элементов реализовывать через __add__
 # Если результат не определен - то возвращать None
@@ -19,7 +12,143 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+# Таблица преобразований:
+#   Вода + Воздух = Шторм
+#   Вода + Огонь = Пар
+#   Вода + Земля = Грязь
+#   Воздух + Огонь = Молния
+#   Воздух + Земля = Пыль
+#   Огонь + Земля = Лава
+
+
+class Water:
+
+    def __init__(self):
+        self.name = 'Вода'
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if other.name == Air().name:
+            return Storm()
+        elif other.name == Fire().name:
+            return Vapor()
+        elif other.name == Ground().name:
+            return Mud()
+        else:
+            return None
+
+
+class Air:
+
+    def __init__(self):
+        self.name = 'Воздух'
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if other.name == Fire().name:
+            return Lightning()
+        elif other.name == Ground().name:
+            return Dust()
+        elif other.name == Water().name:
+            return Storm()
+        else:
+            return None
+
+
+class Fire:
+
+    def __init__(self):
+        self.name = 'Огонь'
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if other.name == Ground().name:
+            return Lava()
+        elif other.name == Air().name:
+            return Lightning()
+        elif other.name == Water().name:
+            return Vapor()
+        else:
+            return None
+
+
+class Ground:
+
+    def __init__(self):
+        self.name = 'Земля'
+
+    def __str__(self):
+        return self.name
+
+
+class Storm:
+
+    def __init__(self):
+        self.name = 'Шторм'
+
+    def __str__(self):
+        return self.name
+
+
+class Vapor:
+
+    def __init__(self):
+        self.name = 'Пар'
+
+    def __str__(self):
+        return self.name
+
+
+class Mud:
+
+    def __init__(self):
+        self.name = 'Грязь'
+
+    def __str__(self):
+        return self.name
+
+
+class Lightning:
+
+    def __init__(self):
+        self.name = 'Молния'
+
+    def __str__(self):
+        return self.name
+
+
+class Dust:
+
+    def __init__(self):
+        self.name = 'Пыль'
+
+    def __str__(self):
+        return self.name
+
+
+class Lava:
+
+    def __init__(self):
+        self.name = 'Лава'
+
+    def __str__(self):
+        return self.name
+
+
+print(Water(), '+', Air(), '=', Water() + Air())
+print(Water(), '+', Fire(), '=', Water() + Fire())
+print(Water(), '+', Ground(), '=', Water() + Ground())
+print(Air(), '+', Fire(), '=', Air() + Fire())
+print(Air(), '+', Ground(), '=', Air() + Ground())
+print(Fire(), '+', Ground(), '=', Fire() + Ground())
+
+print(Air(), '+', Water(), '=', Air() + Water())
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.

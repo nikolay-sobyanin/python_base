133ebf02e9540be17a6f5174ce164a2a3a5e9d28
Nikolay <kolya-sob@yandex.ru>
2021-01-26 22:52:50 +0300

    Lesson 9. Exercise 02. Parser by minute.

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -22,8 +22,61 @@
 # Для этого пригодится шаблон проектирование "Шаблонный метод"
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
+from collections import defaultdict
 
-# TODO здесь ваш код
+
+class LogParser:
+
+    def __init__(self, file_name_in, file_name_out):
+        self.file_name_in = file_name_in
+        self.file_name_out = file_name_out
+        self.list_log = []
+        self.list_result = defaultdict(int)
+
+    def parser(self):
+        self.read_file()
+        self.group_events()
+        self.write_result_file()
+
+    def read_file(self):
+        with open(file=self.file_name_in, mode='r', encoding='utf8') as file:
+            for line in file:
+                date = line[1:11]
+                time = line[12:17]
+                status = line[29:-1]
+                year = date[0:4]
+                month = date[5:7]
+                day = date[8:10]
+                hour = time[0:2]
+                minute = time[3:5]
+                self.list_log.append({
+                    'year': year,
+                    'month': month,
+                    'day': day,
+                    'hour': hour,
+                    'minute': minute,
+                    'status': status,
+                })
+
+    def group_events(self):
+        for log in self.list_log:
+            time_log = f'[{log["year"]}-{log["month"]}-{log["day"]} {log["hour"]}:{log["minute"]}]'
+            if log['status'] == 'NOK':
+                self.list_result[time_log] += 1
+
+    def write_result_file(self):
+        with open(file=self.file_name_out, mode='w', encoding='utf8') as file:
+            for time, res in self.list_result.items():
+                line = f'{time} {res}\n'
+                file.write(line)
+
+
+file_name_in = 'events.txt'
+file_name_out = 'out.txt'
+LogParser(file_name_in=file_name_in, file_name_out=file_name_out).parser()
+
+
+
 
 # После зачета первого этапа нужно сделать группировку событий
 #  - по часам

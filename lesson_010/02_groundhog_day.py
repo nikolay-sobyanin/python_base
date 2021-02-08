cc9a99c0c708586780c38e3a2ebb3533561c6e93
Nikolay <kolya-sob@yandex.ru>
2021-02-08 17:56:35 +0300

    Lesson 10. Fixed 01 and 02 exercise.

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -21,51 +21,43 @@ import os
 
 
 class IamGodError(Exception):
-    pass
+    def __str__(self):
+        return 'Я бог!'
 
 
 class DrunkError(Exception):
-    pass
+    def __str__(self):
+        return 'Я пил!'
 
 
 class CarCrashError(Exception):
-    pass
+    def __str__(self):
+        return 'Я попал в аварию!'
 
 
 class GluttonyError(Exception):
-    pass
+    def __str__(self):
+        return 'Я переел!'
 
 
 class DepressionError(Exception):
-    pass
+    def __str__(self):
+        return 'Я в депрессии!'
 
 
 class SuicideError(Exception):
-    pass
-
-    # TODO: если у классов реализовать метод __str__, который будет возвращать сообщение об ошибке, то
-    #  список ниже можно будет превратить в список (а лучше кортеж) классов.
-    #  Мы же чаще делаем:
-    #       raise ValueError        # класс
-    #  а не:
-    #       raise ValueError()      # объект
-    #  .
-    #  К тому же нельзя будет создать по ошибке или случайно такое исключение:
-    #       IamGodError('Я пил!')
-    #       DrunkError('Я пил!')
-    #  .
-    #  Можно конечно и как сейчас оставить, но тогда контр-аргументы в студию)
+    def __str__(self):
+        return 'Я думал о суициде!'
 
 
-
-list_exception = [
-        IamGodError('Я бог!'),
-        DrunkError('Я пил!'),
-        CarCrashError('Я попал в аварию!'),
-        GluttonyError('Я переел!'),
-        DepressionError('Я в депрессии!'),
-        SuicideError('Я думал о суициде!')
-    ]
+list_exception = (
+    IamGodError,
+    DrunkError,
+    CarCrashError,
+    GluttonyError,
+    DepressionError,
+    SuicideError,
+)
 
 
 def one_day():
@@ -86,7 +78,7 @@ while carma_level < ENLIGHTENMENT_CARMA_LEVEL:
     day += 1
     try:
         carma_level += one_day()
-    except (IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError, SuicideError) as exc:
+    except list_exception as exc:
         carma_level -= 20
         with open(log_file_name, 'a') as log_file:
             log_file.write(f'День № {day:0>3}. Уровень кармы {carma_level:0>3}. {exc}\n')

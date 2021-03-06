fe3b40949953592008d4370fb1f8c07b8111888d
Nikolay <kolya-sob@yandex.ru>
2021-03-06 22:54:50 +0300

    Lesson 13. Exercise 01. Part two.

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -9,14 +9,11 @@
 # Подходящий шрифт искать на сайте ofont.ru
 
 
-
-
-# TODO: не используемый импорт.
-import os
+import argparse
 from PIL import Image, ImageDraw, ImageFont, ImageColor
 
 
-def make_ticket(fio, from_, to, date):
+def make_ticket(fio, from_, to, date, save_to):
     data = {
         fio.upper(): (45, 125),
         from_.upper(): (45, 195),
@@ -32,18 +29,15 @@ def make_ticket(fio, from_, to, date):
     ticket.show()
     save_ticket = input('Сохранить билет (y/n)? ')
     if save_ticket.lower() in ['yes', 'y', 'да']:
-        ticket.save('ticket.png')
+        ticket.save(save_to)
         print(f'Билет сохранен!')
     else:
         print('Билет не сохранен.')
 
 
-make_ticket(fio='Константин', from_='Moscow', to='Perm', date='19.04')
+# make_ticket(fio='Константин', from_='Moscow', to='Perm', date='19.04', save_to='new_ticket.png')
 
 
-
-
-# TODO: можно доп.часть
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля argparse.
 # Скрипт должен принимать параметры:
@@ -53,3 +47,18 @@ make_ticket(fio='Константин', from_='Moscow', to='Perm', date='19.04')
 #   --date - обязательный, когда летим.
 #   --save_to - необязательный, путь для сохранения заполненнего билета.
 # и заполнять билет.
+
+
+parser = argparse.ArgumentParser(description="Make new ticket")
+parser.add_argument('--fio', required=True, type=str, help='Name')
+parser.add_argument('--from_', required=True, type=str, help='From')
+parser.add_argument('--to', required=True, type=str, help='To')
+parser.add_argument('--date', required=True, type=str, help='Date')
+parser.add_argument('--save_to', default='new_ticket.png', type=str, help='Save to...')
+args = vars(parser.parse_args())
+
+make_ticket(**args)
+
+# Для запуска скрипта.
+# Нужно в cmd консоли перейти в папку где расположен скрипт (cd <path>), и выполнить команду:
+# python 01_ticket.py --fio Nick --from Moscow --to Perm --date 19.04

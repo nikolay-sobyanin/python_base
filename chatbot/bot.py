9d400d7683a0dbc52929628f4a089bf62053e6f4
Nikolay <kolya-sob@yandex.ru>
2021-03-22 13:40:00 +0300

    Chat bot.

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -1,25 +1,43 @@
 # -*- coding: utf-8 -*-
 import logging
-
-from _token import token
-
 import random
 import vk_api
 from vk_api import bot_longpoll
 
+try:
+    from local_config import GROUP_ID, TOKEN
+except ImportError:
+    exit('Do cp local_config.py.default local_config.py and set token')
+
+
 # TODO: сделайте отдельный файл local_config.py
 #  В него вынесите id группы и токен. Сам файл добавьте в гит.игнор.
-group_id = 203294647
 
 log = logging.getLogger('bot')
-stream_handler = logging.StreamHandler()
-log.addHandler(stream_handler)
 log.setLevel(logging.DEBUG)
-stream_handler.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler(filename='bot.log', encoding="utf8")
+file_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+file_handler.setLevel(logging.DEBUG)
+
+log.addHandler(stream_handler)
+log.addHandler(file_handler)
+
 
 class Bot:
-
+    """
+    Echo bot for vk.com
+    Use Python 3.8
+    """
     def __init__(self, group_id, token):
+        """
+        :param group_id: group id from vk.com
+        :param token:  secret token
+        """
         self.group_id = group_id
         self.token = token
 
@@ -27,35 +45,39 @@ class Bot:
         #  У VkApi есть параметр api_version, который по умолчанию стоит на 5.92 (это старая динозавр-версия)
         #  Не забудьте в группе поменять версию API.
         #  Так же учтите, что event в run изменится. Текст сообщения начнет хранится в другом поле!
+        # Я в самом начале переключил на версию 5.130. Я изменил уже путь к тексту сообщения.
         self.vk = vk_api.VkApi(token=self.token)
         self.long_poller = bot_longpoll.VkBotLongPoll(self.vk, self.group_id)
 
         self.api = self.vk.get_api()
 
     def run(self):
+        """Run bot"""
         for event in self.long_poller.listen():
             try:
                 self.on_event(event)
             except Exception:
                 log.exception('Ошибка в обработке события.')
-                # print(exc)
 
     def on_event(self, event):
+        """
+        Send massage back if it's text
+        :param event: VkBotMessageEvent object
+        :return: None
+        """
         if event.type == bot_longpoll.VkBotEventType.MESSAGE_NEW:
             self.api.messages.send(
                 message=event.object.message['text'],
                 random_id=random.randint(0, 2 ** 20),
                 peer_id=event.object.message['peer_id']
             )
-            log.info('Отправили сообщение назад.')
-            # print('Ты сказал:', event.object.message['text'])
+            log.debug('Отправили сообщение назад.')
         else:
             log.debug(f'Мы пока не умеем обрабатывать события тип {event.type}')
-            # print(f'Мы пока не умеем обрабатывать события тип {event.type}')
 
 
 def main():
-    bot = Bot(group_id, token)
+    bot = Bot(GROUP_ID, TOKEN)
     bot.run()
 
 
